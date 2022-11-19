557ffeb0c22d086be906914c8e3c6b70c3de3de3
Gerardo <gerardodiletizia@gmail.com>
2022-11-19 20:04:47 +0100

    filter categories and add cart

--- assets/js/scriptProducts.js
+++ assets/js/scriptProducts.js
@@ -2,13 +2,59 @@
 
 var cards;
 var list = [];
+var list2 = [];
 var url = window.location.href;
+var cat = Number(url.slice(url.indexOf('=') + 1, url.length));
+var radio, radio1, radio2, radio3, radio4;
+var cartArticles = [];
 
-window.addEventListener("DOMContentLoaded", init);
+window.addEventListener("DOMContentLoaded", initProducts);
 
-function init() {
+function initProducts() {
+    // initCart();
     cards = document.getElementById("productCards");
+    radio = document.getElementById("radioAll");
+    radio1 = document.getElementById("radioGuitars");
+    radio2 = document.getElementById("radioPianos");
+    radio3 = document.getElementById("radioViolins");
+    radio4 = document.getElementById("radioDrums");
+    switchCat();
+    eventHandlerProducts();
     printData();
+}
+
+function switchCat() {
+    switch (cat) {
+        case 1: radio1.checked = true;
+            break;
+        case 2: radio2.checked = true;
+            break;
+        case 3: radio3.checked = true;
+            break;
+        case 4: radio4.checked = true;
+            break;
+        default: radio.checked = true;
+            break;
+    }
+}
+
+function eventHandlerProducts() {
+
+    radio.addEventListener('change', () => {
+        location.href = "products.html?cat=0";
+    });
+    radio1.addEventListener('change', () => {
+        location.href = "products.html?cat=1";
+    });
+    radio2.addEventListener('change', () => {
+        location.href = "products.html?cat=2"
+    });
+    radio3.addEventListener('change', () => {
+        location.href = "products.html?cat=3"
+    })
+    radio4.addEventListener('change', () => {
+        location.href = "products.html?cat=4"
+    })
 }
 
 function printData() {
@@ -18,18 +64,63 @@ function printData() {
         })
         .then((data) => {
             list = data;
-            if (list.length > 0) {
-                cards.innerHTML = '';
-                list.map(function (element) {
+            list.forEach((element) => {
+                if (element.idCat == cat || cat == 0) {
                     cards.innerHTML += `
-                    <div class="card border border-white align-content-between" onclick="location.href='detail.html?id=${element.id}'">
-                        <div class="text-center"><img src="${element.image}" class="card-img-top px-3 pt-3"></div>
+                    <div class="card border border-white align-content-between">
+                        <div class="text-center"><img src="${element.image}" class="card-img-top px-3 pt-3" onclick="location.href='detail.html?id=${element.id}'"></div>
                         <div class="card-body text-center d-flex flex-column justify-content-end cardText">
-                            <h5 class="card-title">${element.title}</h5>
+                            <h5 class="card-title" onclick="location.href='detail.html?id=${element.id}'">${element.title}</h5>
                             <p class="fs-5">${element.price}&euro;</p>
                         </div>
+                        <div><button type="button" class="btn btn-warning" onclick="addProd(${element.id})">Add to Cart</button></div>
                     </div>`
-                });
-            }
+                }
+            });
         });
+}
+
+async function addProd(product) {
+
+    var uID = Number(localStorage.getItem('userId'));
+    var cID = Number(localStorage.getItem('cartId'));
+    fetch("http://localhost:3000/cart/")
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            list2 = data;
+            list2.forEach((element) => {
+                if (element.id == cID) {
+                    cartArticles = element.articles;
+                    console.log(element.articles);
+                    let cart = {
+                        idUser: uID,
+                        articles: []
+                    }
+                    cartArticles.push(product);
+                    cart.articles = cartArticles;
+                    console.log('nuovo carrello');
+                    console.log(cart.articles);
+
+                    updateCart(cart, cID);
+
+                }
+            });
+
+
+        });
+
+
+
+
+
+}
+
+async function updateCart(cart, cID) {
+    let response = await fetch("http://localhost:3000/cart/" + cID, {
+        method: "PUT",
+        headers: { 'Content-Type': 'application/json;charset=utf-8' },
+        body: JSON.stringify(cart),
+    })
 }
